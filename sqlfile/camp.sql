9a75b6ce0db0aec9a4a7d35198450eea1a581a2c
CR <newxiaoraotest@outlook.com>
2024-03-05 19:07:04 +0800

    更新重构后的数据库sql

--- sqlfile/camp.sql
+++ sqlfile/camp.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50743
  File Encoding         : 65001
 
- Date: 15/01/2024 14:52:54
+ Date: 05/03/2024 19:05:06
 */
 
 SET NAMES utf8mb4;
@@ -32,7 +32,7 @@ CREATE TABLE `admin`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `account_UNIQUE`(`account`) USING BTREE,
   UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '管理员账号表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '管理员账号表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for application_logs
@@ -41,9 +41,6 @@ DROP TABLE IF EXISTS `application_logs`;
 CREATE TABLE `application_logs`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '申请记录ID',
   `stuID` char(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '学号',
-  `classID` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '教师工号',
-  `name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '学生姓名',
-  `className` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '班级名',
   `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '通行理由',
   `state` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '审核状态(0：待审,1：通过，2：未通过，3：已撤销)',
   `time` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '通行次数',
@@ -53,7 +50,7 @@ CREATE TABLE `application_logs`  (
   `tcRemark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '审核备注',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '通行申请记录表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '通行申请记录表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for class
@@ -62,11 +59,9 @@ DROP TABLE IF EXISTS `class`;
 CREATE TABLE `class`  (
   `id` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '班级号',
   `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '班级名',
-  `tcID` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '班主任账号/工号',
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE,
-  INDEX `tcid_idx`(`tcID`) USING BTREE
+  UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '班级信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -76,7 +71,8 @@ DROP TABLE IF EXISTS `face`;
 CREATE TABLE `face`  (
   `id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '工号/学号\n不可重复',
   `features` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '人脸特征信息',
-  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `facepic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '人脸照片url',
+  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '信息记录生成日期',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '人脸信息表' ROW_FORMAT = Dynamic;
@@ -97,21 +93,20 @@ CREATE TABLE `students`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `account_UNIQUE`(`account`) USING BTREE,
   UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE,
-  INDEX `classID_idx`(`classID`) USING BTREE,
-  CONSTRAINT `cid` FOREIGN KEY (`classID`) REFERENCES `class` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '学生账号表' ROW_FORMAT = Dynamic;
+  INDEX `classID_idx`(`classID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '学生账号表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for teacher_class_map
 -- ----------------------------
 DROP TABLE IF EXISTS `teacher_class_map`;
 CREATE TABLE `teacher_class_map`  (
-  `id` int(11) NOT NULL COMMENT '主键',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `teacher_id` int(11) NULL DEFAULT NULL COMMENT '教师ID',
   `class_id` int(11) NULL DEFAULT NULL COMMENT '班级ID',
-  `is_master` tinyint(1) UNSIGNED NULL DEFAULT 0 COMMENT '是否是班主任',
+  `is_master` tinyint(1) UNSIGNED NULL DEFAULT 0 COMMENT '是否是班主任(1是  0否)',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for teachers
@@ -122,14 +117,18 @@ CREATE TABLE `teachers`  (
   `account` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账号/工号',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
   `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '姓名',
-  `classID` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '班级号',
   `email` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
   `pic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像信息',
-  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `account_UNIQUE`(`account`) USING BTREE,
-  UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE,
-  INDEX `classID_idx`(`classID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 83 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '教师账号表' ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `id_UNIQUE`(`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 78 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '教师账号表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- View structure for view_application_logs
+-- ----------------------------
+DROP VIEW IF EXISTS `view_application_logs`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `view_application_logs` AS select `application_logs`.`id` AS `id`,`application_logs`.`stuID` AS `stuID`,`application_logs`.`reason` AS `reason`,`application_logs`.`state` AS `state`,`application_logs`.`time` AS `time`,`application_logs`.`validityDate` AS `validityDate`,`application_logs`.`applicationDate` AS `applicationDate`,`application_logs`.`reviewDate` AS `reviewDate`,`application_logs`.`tcRemark` AS `tcRemark`,`students`.`name` AS `name`,`students`.`classID` AS `classID`,`class`.`name` AS `className` from ((`application_logs` left join `students` on((`application_logs`.`stuID` = `students`.`account`))) left join `class` on((`class`.`id` = `students`.`classID`)));
 
 SET FOREIGN_KEY_CHECKS = 1;
